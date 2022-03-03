62774f2b9161d5b9bfa22c3317b22c71ece75715
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2022-03-03 13:32:13 +0100

    estetica login + inicio navbar

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,77 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pruebas</title>
+    
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/prueba.css') }}">
+</head>
+<body>
+    <div class="contenedor">
+        <nav>
+            <div class="nav-content"></div>
+            <div class="nav-decoration">
+                <div class="wave wave1"></div>
+                <div class="wave wave2"></div>
+                <div class="wave wave3"></div>
+                <div class="wave wave4"></div>
+            </div>
+        </nav>
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+        <div class="loginCard">
+            <div id="form1" class="form1">
+                <h1 class="noSaltar">Iniciar Sesi&oacute;n</h1>
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <input class="form-control" id="username" type="text" placeholder="Nombre de usuario" name="username" value="{{ old('username') }}" required>
+                    <input class="form-control" id="password" type="password" placeholder="Contrase&ntilde;a" name="password" required>
+                    <div class="form-check noSaltar">
+                        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                        <label class="form-check-label" for="remember">
+                            {{ __('Recordarme') }}
+                        </label>
+                    </div>
+                    
+                    @if (Route::has('password.request'))
+                        <a class="btn btn-link" href="{{ route('password.request') }}">
+                            {{ __('¿Olvidaste tu contraseña?') }}
+                        </a>
+                    @endif
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                    <button type="submit" class="btn btn-primary col-12">
+                        {{ __('Iniciar sesión') }}
+                    </button>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                                
+                </form>
+            </div>
+            <div id="form2" class="form2 inactiveForm">
+                <h1>Registrarse</h1>
+                <form method="POST" action="{{ route('register') }}">
+                    @csrf
+                    <input id="name" type="text" class="form-control" name="nombre" value="{{ old('nombre') }}" placeholder="Nombre" required>
+                    <input class="form-control" id="username" type="text" placeholder="Nombre de usuario" name="username" value="{{ old('username') }}" required>
+                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email" required>
+                    <input type="text" id="direccion" class="form-control" name="direccion" value="{{ old('direccion') }}" placeholder="Direcci&oacute;n">
+                    <input class="form-control" type="tel" name="telefono" id="telefono" placeholder="N&uacute;mero de tel&eacute;fono">
+                    <div class="input-group">
+                        <input class="form-control" id="password" type="password" placeholder="Contrase&ntilde;a" name="password" required>
+                        <span class="input-group-text" id="basic-addon1"></span>
+                        <input id="password-confirm" type="password" class="form-control" placeholder="Repetir contrase&ntilde;a" name="password_confirmation" required autocomplete="new-password">
+                    </div>
+                    
+                    <button type="submit" class="btn btn-primary col-12">
+                        {{ __('Registrarse') }}
+                    </button>
+                </form>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <script src="{{ asset('js/prueba.js') }}"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
