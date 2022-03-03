0a1c02df6f83751567c8c2075cc28fddf6daf622
Danie Tamargo <danieltamargosaiz@gmail.com>
2022-03-03 13:25:28 +0100

    Controllers vacíos

---
+++ app/Http/Controllers/ProductosPedidoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductosPedido;
+use Illuminate\Http\Request;
+
+class ProductosPedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductosPedido  $productosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductosPedido $productosPedido)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductosPedido  $productosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductosPedido $productosPedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductosPedido  $productosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductosPedido $productosPedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductosPedido  $productosPedido
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductosPedido $productosPedido)
+    {
+        //
+    }
+}
