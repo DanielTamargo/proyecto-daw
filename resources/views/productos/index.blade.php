4d570883b3a2a58c964a7d0ee334382f36c97394
Raul-Melgosa <raul.melgosa@ikasle.egibide.org>
2022-03-09 10:22:23 +0100

    pagina index inicial completa

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('styleScripts')
-
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 @endsection
 
 
@@ -9,21 +9,7 @@
     <div class="contenedor justify-content-start">
         <div class="contenedor-categorias col-12"></div>
         <div class="contenedor-productos col-12">
-            <style>
-                .nombre {
-                    border-bottom: 1.5px solid white;
-                }
-
-                .precio{
-                    font-size: 2em;
-                    transition: .4s;
-                }
-
-                .contenedor-productos > div:hover > .contenedor-info .precio {
-                    font-size: 4em;
-                }
-            </style>
-        @for ($i = 0; $i <= 1500; $i+=5)
+        @for ($i = 0; $i <= 500; $i+=5)
             <div>
                 
                 <div class="contenedor-imagen">
@@ -32,7 +18,27 @@
                 <div class="contenedor-info">
                     <p class="nombre col-8 text-center fw-bold pb-2 fs-4 m-0">Gatito</p>
                     <p class="precio">17.99€</p>
-                    <div class="botones-carrito"></div>
+                    <div class="annadir-carrito">
+                        <div class="btn-menos">
+                            <button><b>-</b></button>
+                        </div>
+                        <div class="btn-principal">
+                            <button class="add-to-cart">
+                                <div class="default">Añadir al carrito</div>
+                                <div class="success">Añadido</div>
+                                <div class="cart">
+                                    <div>
+                                        <div></div>
+                                        <div></div>
+                                    </div>
+                                </div>
+                                <div class="dots"></div>
+                            </button>
+                        </div>
+                        <div class="btn-mas">
+                            <button><b>+</b></button>
+                        </div>
+                    </div>
                 </div>
                 
             </div>
@@ -44,5 +50,15 @@
 
 
 @section('scripts')
-
+<script>
+    document.querySelectorAll('.add-to-cart').forEach(button => {
+        let btnMenos = button.parentElement.parentElement.children[0];
+        let btnMas = button.parentElement.parentElement.children[2];
+        button.addEventListener('click', e => {
+            button.classList.toggle('added');
+            btnMenos.classList.toggle('visible');
+            btnMas.classList.toggle('visible');
+        });
+    });
+</script>
 @endsection
