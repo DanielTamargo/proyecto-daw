7740fc294f4b916c382a09c1311edab526e41f47
Danie Tamargo <danieltamargosaiz@gmail.com>
2022-03-14 12:02:45 +0100

    Seeder con estilo

--- database/factories/ProductoFactory.php
+++ database/factories/ProductoFactory.php
@@ -24,12 +24,39 @@ class ProductoFactory extends Factory
 
         $admins = User::where('rol', Constants::ROL_ADMINISTRADOR)->get();
 
+        $numPlaceholder = rand(1, 8);
+        switch($numPlaceholder) {
+            case 2:
+                $nombre = 'Tortilla';
+                break;
+            case 3:
+                $nombre = 'Salmón';
+                break;
+            case 4:
+                $nombre = 'Crema de espárragos';
+                break;
+            case 5:
+                $nombre = 'Solomillo';
+                break;
+            case 6:
+                $nombre = 'Tarta';
+                break;
+            case 7:
+                $nombre = 'Pulpo';
+                break;
+            case 8:
+                $nombre = 'Spaguetti a la';
+                break;
+            default:
+                $nombre = 'Hamburguesa';
+        }
+
         // Requiere recibir de un valor creado_por, que será el id de algún administrador
         return [
-            'nombre' => ucfirst($this->faker->words(rand(3, 5), true)),
+            'nombre' => $nombre . ' ' . $this->faker->words(rand(1, 3), true),
             'descripcion' => $this->faker->realTextBetween(),
             'precio' => $this->faker->randomFloat(2, 2, 20),
-            'foto' => 'placeholder_' . rand(1, 6) . '.png',
+            'foto' => 'placeholder_' . $numPlaceholder . '.png',
             'creado_por' => $admins[rand(0, count($admins) - 1)]->id,
             'fecha_publicacion' => Constants::randomTimestampEntreFechas($fecha_min, $fecha_max),
         ];
